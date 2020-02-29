0d2f825a45df8e00d9ed226fe52ed2953c9b744e
Yi Feng Yang <yifengyang01@gmail.com>
2020-02-29 16:20:17 -0500

    Add initial widget

--- components/HelloWorldWidget.tsx
+++ components/HelloWorldWidget.tsx
@@ -1,6 +1,30 @@
+import resolveProviderFunction, {
+  ProviderType
+} from "../providers/ProviderResolver";
+import { useState, useEffect } from "react";
 
-const HelloWorldWidget = (props) => {
-    return <div><h1>Hello world</h1></div>
+type PropType = {
+  type: ProviderType;
 };
+
+const HelloWorldWidget = (props: PropType) => {
+  const {type} = props;
+  const [helloValue, setHelloValue] = useState("loading...");
+
+  useEffect(() => {
+    (async function() {
+      const finalProviderFunction = resolveProviderFunction(type) || (async () => Promise.resolve({value: "ERROR DURING INITIALIZATION"}));
+      const finalHelloJson = await finalProviderFunction();
+      setHelloValue(finalHelloJson.value);
+    })();
+  }, []);
+  
+  return (
+    <div>
+      <h1>The value is {helloValue}</h1>
+    </div>
+  );
+};
+
 
 export default HelloWorldWidget;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,23 @@
 import HelloWorldWidget from "../components/HelloWorldWidget";
+import {ProviderType} from "../providers/ProviderResolver";
 const Index = () => {
-  return <div>
-    <h1>The most USELESS program in existence</h1>
-
-    <style jsx>
-      {`
-      * {
-        font-family: Lato, Arial, sans-serif;
-      }
-      h1 {
-
-      }
-      `}
-    </style>
-  </div>;
+  return (
+    <div>
+      <h1>The most USELESS program in existence</h1>
+      <section id="theImportantStuff">
+        <HelloWorldWidget type={ProviderType.SERVERLESS}/>
+      </section>
+      <style jsx>
+        {`
+          * {
+            font-family: Lato, Arial, sans-serif;
+          }
+          h1 {
+          }
+        `}
+      </style>
+    </div>
+  );
 };
 
 export default Index;
